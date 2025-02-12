f4e7d17ee0c1e3924eada37e8a5f68a555cd5b23
Miracle <002nasya@gmail.com>
2025-02-12 11:19:34 +0200

    remove unused create_app function from __init__.py

--- backend/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-
-    from backend.routes import app_routes  # Import after app is created
-    app.register_blueprint(app_routes)
-
-    return app
